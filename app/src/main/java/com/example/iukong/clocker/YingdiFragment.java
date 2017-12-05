a6a36dcd967a8916d0a113b672d6dff69e704d8e
mm0rys <mmorysXQL@gmail.com>
2017-12-05 21:26:37 +0800

    version 1.1

--- app/src/main/java/com/example/iukong/clocker/YingdiFragment.java
+++ app/src/main/java/com/example/iukong/clocker/YingdiFragment.java
@@ -1,16 +1,28 @@
 package com.example.iukong.clocker;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by 和iukong、 on 2017/11/13.
@@ -20,6 +32,8 @@ public class YingdiFragment extends Fragment {
 
     private int recLen = 0;
     private TextView txtView;
+    private ArrayList<String> list = new ArrayList<>();
+    private SwipeMenuListView listView;
 
     @Nullable
     @Override
@@ -30,12 +44,24 @@ public class YingdiFragment extends Fragment {
 
         Button btn = (Button) RootView.findViewById(R.id.button_vibrate);
         txtView = (TextView) RootView.findViewById(R.id.txttime);
+        FloatingActionButton floatingActionButton = (FloatingActionButton) RootView.findViewById(R.id.floatingActionButton_add);
+        listView = (SwipeMenuListView) RootView.findViewById(R.id.SwipeMenuListView);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("Test","震动被点击");
                 Toast.makeText(getActivity(),"开始计时",Toast.LENGTH_SHORT).show();
                 handler.postDelayed(runnable, 1000);
+            }
+        });
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                list.add("test");
+                ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1,list);
+                listView.setAdapter(adapter);
+                // set creator
+                listView.setMenuCreator(creator);
             }
         });
 
@@ -51,4 +77,40 @@ public class YingdiFragment extends Fragment {
             handler.postDelayed(this, 1000);
         }
     };
+    SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+        @Override
+        public void create(SwipeMenu menu) {
+            // create "open" item
+            SwipeMenuItem openItem = new SwipeMenuItem(getActivity());
+            // set item background
+            openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                    0xCE)));
+            // set item width
+            openItem.setWidth(170);
+            // set item title
+            openItem.setTitle("Open");
+            // set item title fontsize
+            openItem.setTitleSize(18);
+            // set item title font color
+            openItem.setTitleColor(Color.WHITE);
+            // add to menu
+            menu.addMenuItem(openItem);
+
+            // create "delete" item
+            SwipeMenuItem deleteItem = new SwipeMenuItem(
+                    getActivity());
+            // set item background
+            deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                    0x3F, 0x25)));
+            // set item width
+            deleteItem.setWidth(120);
+            // set a icon
+            deleteItem.setIcon(R.drawable.ic_delete);
+            // add to menu
+            menu.addMenuItem(deleteItem);
+        }
+    };
+
+
 }
